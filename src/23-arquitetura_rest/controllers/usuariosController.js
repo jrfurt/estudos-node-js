061d1129d446c916aea313be974f2a2be2fc37af
Clodomir Jr <106870350+jrfurt@users.noreply.github.com>
2024-02-10 11:30:23 -0300

    meelhorias no controllers e models

--- src/23-arquitetura_rest/controllers/usuariosController.js
+++ src/23-arquitetura_rest/controllers/usuariosController.js
@@ -1,44 +1,56 @@
 const usuariosModel = require('../models/usuariosModel');
 
 // READ
-const getAll = async(req, res) => {
-    const [row] = await usuariosModel.getAll();
-    return res.json(row);
+async function getAll(req, res) {
+    usuariosModel.getAll()
+        .then((result) => {
+            const [row] = result;
+            return res.status(200).json(row);
+        })
+        .catch(() => {
+            return res.status(500).json({ message: 'Ocorreu um erro interno ao buscar' });
+        });
 }
 
 // CREATE
-const createUsuario = async(req, res) => {
+// * melhorar a tratativa de erro *
+async function createUsuario(req, res) {
     const { nickname, senha } = req.body;
 
+    if (!nickname || !senha) {
+        return res.status(400).json({ message: 'Usuario e senha nao podem ser vazios' });
+    }
+
     const loginID = await usuariosModel.create(nickname, senha);
-    return res.status(201).json({
-        message: "Login criado com sucesso.",
-        loginID
-    });
+    return res.status(201).json({ message: 'Login criado com sucesso.', loginID });
 }
 
 // DELETE
-const deleteUsuario = (req, res) => {
+function deleteUsuario(req, res) {
     const { id } = req.params;
 
     usuariosModel.deleteLogin(id)
-        .then(() => res.status(200).json({ message: "Usuario deletado com sucesso" }))
-        .catch((err) => res.status(500).json({ message: err }))
+        .then(() => {
+            return res.status(200).json({ message: 'Login deletado com sucesso' });
+        })
+        .catch((err) => {
+            return res.status(500).json({ message: 'Ocorreu um erro ao deletar login' });
+        });
 }
 
 // UPDATE
-const editUsuario = (req, res) => {
+function editUsuario(req, res) {
     const { nickname, senha } = req.body;
     const { id } = req.params;
 
     usuariosModel.update(nickname, senha, id)
         .then(() => {
-            return res.status(200).json({ message: "Dados atualizados com sucesso" })
-        }) 
-        .catch(() => {
-            return res.status(500).json({ erro: "Occorreu um erro ao atualizar login" })
+            return res.status(200).json({ message: 'Dados atualizados com sucesso' });
         })
-    
+        .catch(() => {
+            return res.status(500).json({ erro: 'Occorreu um erro ao atualizar login' });
+        });
+
 }
 
 module.exports = {
--- src/23-arquitetura_rest/models/usuariosModel.js
+++ src/23-arquitetura_rest/models/usuariosModel.js
@@ -1,28 +1,26 @@
 const db = require('../../db/conexao-async');
 
-const getAll = () => {
-    return db.execute('SELECT * FROM logins')
-        .then((result) => result)
-        .catch((err) => err)
+function getAll() {
+    return db.execute('SELECT * FROM logins');
 } 
 
-const create = async(nickname, senha) => {
-    const sql = "INSERT INTO logins (nickname, senha) VALUES (?, ?);"
-    const values = [nickname, senha]
-    const [{ insertId }] = await db.execute(sql, values)
+async function create(nickname, senha) {
+    const sql = "INSERT INTO logins (nickname, senha) VALUES (?, ?);";
+    const values = [nickname, senha];
+    const [{ insertId }] = await db.execute(sql, values);
 
-    return insertId
+    return insertId;
 }
 
-const deleteLogin = (id) => {
-    const sql = "DELETE FROM logins WHERE id = ?;"
-    return db.execute(sql, [id])
+function deleteLogin(id) {
+    const sql = "DELETE FROM logins WHERE id = ?;";
+    return db.execute(sql, [id]);
 }
 
-const update = (nickname, senha, id) => {
-    const sql = "UPDATE logins SET nickname = ?, senha = ? WHERE id = ?;"
-    const values = [nickname, senha, id]
-    return db.execute(sql, values)
+function update(nickname, senha, id) {
+    const sql = "UPDATE logins SET nickname = ?, senha = ? WHERE id = ?;";
+    const values = [nickname, senha, id];
+    return db.execute(sql, values);
 }
 
 module.exports = {
